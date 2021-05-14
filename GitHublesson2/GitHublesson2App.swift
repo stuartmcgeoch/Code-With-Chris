74d22e20d9b2629ed9f85e9a5449adbe86aada9b
Stuart McGeoch <stuartmcgeochme.com>
2021-05-14 13:31:45 -0500

    Initial Commit

---
+++ GitHublesson2/GitHublesson2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHublesson2App.swift
+//  GitHublesson2
+//
+//  Created by Stuart on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHublesson2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
